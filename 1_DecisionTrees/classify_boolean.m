c54d9bff085c9012baebc3eb426115d941bbb1dd
lawliet89 <lawliet89@gmail.com>
2013-10-17 17:33:30 +0100

    Modify functions to always generate a matrix

--- 1_DecisionTrees/classify_boolean.m
+++ 1_DecisionTrees/classify_boolean.m
@@ -1,9 +1,18 @@
-% Using a SINGLE tree, generate a yes/no answer
-function result = classify_boolean(tree, x)
+% Using a single tree, generate a yes/no answer for items
+function y = classify_boolean(tree, x)
+    y = nan(size(x, 1), 1);
+    for i = 1:size(x, 1)
+       y(i) = classify_boolean_single_input(tree, x(i));
+    end
+end
+
+% Using a SINGLE tree, generate a yes/no answer for a single item
+function result = classify_boolean_single_input(tree, x)
     if is_leaf(tree)
         result = tree.class;
     else
-        result = classify_boolean(tree.kids{x(1, tree.op) + 1}, x);
+        result = classify_boolean_single_input...
+            (tree.kids{x(1, tree.op) + 1}, x);
     end
 end
 
--- 1_DecisionTrees/classify_boolean_all.m
+++ 1_DecisionTrees/classify_boolean_all.m
@@ -1,10 +1,10 @@
+% Using all trees, generate a matrix with the yes/no decision
+% for all the trees
 function y = classify_boolean_all(all_trees, x)
     y = nan(size(x, 1), size(all_trees, 1));
 
     for tree_i = 1: size(all_trees, 1)
         tree = all_trees(tree_i);
-        for i = 1:size(x, 1)
-           y(i, tree_i) = classify_boolean(tree, x(i, :));
-        end
+        y(:, tree_i) = classify_boolean(tree, x);
     end
 end
