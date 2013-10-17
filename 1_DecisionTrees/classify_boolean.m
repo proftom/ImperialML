d7f854a1d493c1ba792b609460b4f408cd89be28
lawliet89 <lawliet89@gmail.com>
2013-10-17 16:48:45 +0100

    Don't check for field existence

--- 1_DecisionTrees/classify_boolean.m
+++ 1_DecisionTrees/classify_boolean.m
@@ -3,10 +3,11 @@ function result = classify_boolean(tree, x)
     if is_leaf(tree)
         result = tree.class;
     else
-        result = classify_boolean(tree.kids{value}, x);
+        result = classify_boolean(tree.kids{x(1, tree.op + 1)}, x);
     end
 end
 
 function result = is_leaf(node)
-    result = exist('node.op') == 0 || node.op == '';
+    % Will blow up if .op does not exist
+    result = node.op == '';
 end
