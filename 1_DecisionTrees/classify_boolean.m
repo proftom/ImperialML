2de11170745892006ace678e2faa3987ccfd4c14
Thomas Morrison <tm1810@ic.ac.uk>
2013-10-17 17:09:13 +0100

    Fix leaf testing and tree.op index

--- 1_DecisionTrees/classify_boolean.m
+++ 1_DecisionTrees/classify_boolean.m
@@ -3,11 +3,11 @@ function result = classify_boolean(tree, x)
     if is_leaf(tree)
         result = tree.class;
     else
-        result = classify_boolean(tree.kids{x(1, tree.op + 1)}, x);
+        result = classify_boolean(tree.kids{x(1, tree.op) + 1}, x);
     end
 end
 
 function result = is_leaf(node)
     % Will blow up if .op does not exist
-    result = node.op == '';
+    result = isempty(node.op);
 end
