226aa51095abfef633e147b9c9fb5493adb995c4
Thomas Morrison <tm1810@ic.ac.uk>
2013-10-17 17:25:10 +0100

    Add semicolon

--- 1_DecisionTrees/classify_boolean_all.m
+++ 1_DecisionTrees/classify_boolean_all.m
@@ -4,7 +4,7 @@ function y = classify_boolean_all(all_trees, x)
     for tree_i = 1: size(all_trees, 1)
         tree = all_trees(tree_i);
         for i = 1:size(x, 1)
-           y(i, tree_i) = classify_boolean(tree, x(i, :)) 
+           y(i, tree_i) = classify_boolean(tree, x(i, :));
         end
     end
 end
