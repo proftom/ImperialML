c6a981bc9494a2b7170d9369983628c54ba83f9a
Thomas Morrison <tm1810@ic.ac.uk>
2013-10-17 16:35:40 +0100

    Created wrapper to train all 6 trees

---
+++ 1_DecisionTrees/generate_all_trees_wrapper.m
@@ -0,0 +1,10 @@
+function tree = generate_all_trees_wrapper(examples, attributes, allTargets) 
+
+    for i = 1:6
+        binary_targets = ml1_transformTargets(i, allTargets);   
+        t(i) = decision_tree_learning(examples, attributes, binary_targets);
+    end
+    
+    tree = t
+    
+end
