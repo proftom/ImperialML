6b75619d701e1e683837ec019b237e1088fa3cae
lawliet89 <lawliet89@gmail.com>
2013-10-21 19:06:47 +0100

    Rename transform targets function and add comments

--- 1_DecisionTrees/generate_all_trees_wrapper.m
+++ 1_DecisionTrees/generate_all_trees_wrapper.m
@@ -1,7 +1,7 @@
 function tree = generate_all_trees_wrapper(examples, attributes, allTargets) 
 
     for i = 1:6
-        binary_targets = ml1_transformTargets(i, allTargets);   
+        binary_targets = transform_targets(i, allTargets);   
         t(i) = decision_tree_learning(examples, attributes, binary_targets);
     end
     
--- 1_DecisionTrees/generate_tree_wrapper.m
+++ 1_DecisionTrees/generate_tree_wrapper.m
@@ -1,6 +1,6 @@
 function tree = generate_tree_wrapper(examples, attributes, allTargets, emotion) 
 
-    binary_targets = ml1_transformTargets(emotion, allTargets);   
+    binary_targets = transform_targets(emotion, allTargets);   
     tree = decision_tree_learning(examples, attributes, binary_targets);
     
 end
