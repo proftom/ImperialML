d45459a10fd902d3aa4270aa84a0338cc7909cdb
lawliet89 <lawliet89@gmail.com>
2013-11-02 13:04:23 +0000

    Simple implementation to generate six output trees

--- 2_ANN/sixOutputAnn.m
+++ 2_ANN/sixOutputAnn.m
@@ -1,5 +1,21 @@
 % Given x and y transformed into the format required using ANNData
-% return an ANN with six outputs for a set of 45-attributes inputs
-function net = sixOutputAnn(x, y)
+% return an ANN with six outputs trained for a set of 45-attributes inputs
+function [net, tr] = sixOutputAnn(x, y, hiddenLayerSize,...
+                                  trainingFunction, maxEpochs)
+    % Handle optional arguments
+    if nargin < 3
+       hiddenLayerSize = 10;
+    end
+    if nargin < 4
+        trainingFunction = 'trainscg';
+    end
+    if nargin < 5
+       maxEpochs = 1000; 
+    end
 
+    % Create the network
+    net = feedforwardnet(hiddenLayerSize, trainingFunction);
+    % Train network
+    net.trainParam.epochs = maxEpochs;
+    [net, tr] = train(net, x, y);
 end
