53e6ac78c6e03083bc1554cd0b51e855e157f731
proftom <tm1810@ic.ac.uk>
2013-11-12 17:21:34 +0000

    Refine makeCase to handle different types of AU inputs

--- 3_CBR/makeCase.m
+++ 3_CBR/makeCase.m
@@ -1,3 +1,13 @@
-function c = makeCase(example, class)
-    c = struct('au', example, 'class', class, 'typicality', 1);
+function c = makeCase(AUs, class)
+        if(length(AUs) < 45 || ((length(AUs) == 45) &&  (any(AUs > 1) == 1)))
+        % Must be a vector of activated AUs
+        au = zeros(1,45);
+        for i = 1:length(AUs)
+           au(AUs(i)) = 1;
+        end
+        c = struct('au', au, 'class', class, 'typicality', 1);
+    else 
+       assert(length(AUs) == 45); 
+       c = struct('au', AUs, 'class', class, 'typicality', 1);           
+    end
 end
--- 3_CBR/makeCaseFromActivatedAUs.m
+++
@@ -1,7 +0,0 @@
-function c = makeCaseFromActivatedAUs(activate_aus, class)
-    au = zeros(1,45);
-    for i = 1:length(activate_aus)
-       au(activate_aus(i)) = 1;
-    end
-    c = struct('au', au, 'class', class, 'typicality', 1);
-end
