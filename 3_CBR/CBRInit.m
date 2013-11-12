a79b65acf8c001d1fda17c3d0599f6655300ff97
Yong Wen Chua <lawliet89@gmail.com>
2013-11-12 12:54:20 +0000

    Initial impelementation

--- 3_CBR/CBRInit.m
+++ 3_CBR/CBRInit.m
@@ -1,9 +1,37 @@
 function [ cbr ] = CBRInit(x,y)
-
-cbr = cell(1,6);
-
-for i=1:size(x,1)
-    cbr{y(i)} = vertcat(cbr{y(i)},x(i,:));
+    noExamples = size(x,1);
+    count = 0;
+    % Pre-allocate arrays for better performance
+    cbr = repmat(makeCase([],[]), noExamples, 1 );
+    
+    for i= 1:size(x, 1)
+        au = x(i, :);
+        class = y(i);
+        found = 0;
+        
+        if count > 0
+            for j = 1:count
+               if all(cbr(j).au == au) && cbr(j).class == class
+                  cbr(j) = incrementTypicality(cbr(j));
+                  found = 1;
+                  break;
+               end
+            end
+        end
+        
+        if found == 0
+            count = count + 1;
+            cbr(count) = makeCase(au, class);
+        end
+    end
+    % Trim the array
+    cbr = cbr(1:count);
 end
 
+function c = makeCase(example, class)
+    c = struct('au', example, 'class', class, 'typicality', 1);
+end
+
+function c = incrementTypicality(c)
+    c.typicality = c.typicality + 1;
 end
