a468e5ce682e496eb7c9dd5a31b0043d3f4de3d8
Yong Wen Chua <lawliet89@gmail.com>
2013-11-12 13:21:38 +0000

    Fix some implementation problems with retrieve

--- 3_CBR/retrieve.m
+++ 3_CBR/retrieve.m
@@ -1,4 +1,4 @@
-function [kase] = retrieve(cbr, newCase)
+function [kase, distances] = retrieve(cbr, newCase)
     % Parameter to retrieve the k-nearest neighbours
     k = 5;
     
@@ -7,12 +7,15 @@ function [kase] = retrieve(cbr, newCase)
        distances(i, 1) = i;
        distances(i, 2) = calculateDistance(cbr(i).au, newCase); 
     end
-    distances = sortrows(distances, 2, 'descend');
+    
+    distances = sortrows(distances, 2);
     
     kase = repmat(makeCase([],[]), k, 1 );
     for i = 1:k
        kase(i) = cbr(distances(i, 1)); 
     end
+    
+    distances = distances(1:k, 2);
 end
 
 function distance = calculateDistance(a, b)
