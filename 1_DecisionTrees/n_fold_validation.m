db961864e07bbfce129a7bd7b50cc8331cc2746c
Yong Wen Chua <lawliet89@gmail.com>
2013-11-20 15:17:18 +0000

    Change decison tree fold validation to calculate confusion matrix per fold

--- 1_DecisionTrees/n_fold_validation.m
+++ 1_DecisionTrees/n_fold_validation.m
@@ -6,7 +6,7 @@
 % generated tree. This produces total size of data / n results each
 % iteration. With each iteration the partitions change.
 % Possible outcomes is the number of unique classifications
-function results = n_fold_validation(examples, classifications,...
+function [results, confusionMatrix] = n_fold_validation(examples, classifications,...
     n, possible_outcomes, strategy)
     % Default to split paritions into 10 
     if nargin < 3
@@ -22,6 +22,9 @@ function results = n_fold_validation(examples, classifications,...
     if nargin < 5
         strategy = 3;
     end
+    
+    % Preallocate for speed
+    confusionMatrix = cell(1,n);
     
     %Length, which forms upper bound
     len = length(examples);
@@ -39,6 +42,8 @@ function results = n_fold_validation(examples, classifications,...
         test_examples = examples((i-1)*partition_size + 1: ...
             i*partition_size, :);
 
+        test_classes = classifications((i-1)*partition_size + 1:i*partition_size, :);
+
 
         % Data used to train the tree
         training_examples = examples([1 : (i-1)*partition_size ... 
@@ -52,40 +57,48 @@ function results = n_fold_validation(examples, classifications,...
             training_examples, training_classifications,...
             possible_outcomes);
 
+        outcomes = classify(emotion_decision_trees,...
+            test_examples, strategy);
+        
         % Concatinate the classifications
         classifcation_results((i-1)*partition_size+1:i*partition_size,:)...
-            = classify(emotion_decision_trees,  test_examples, strategy);
+            = outcomes;
+        
+        confusionMatrix{i} = confusion_matrix(test_classes,outcomes, 6);
 
     end
 
-        lower_bound = (n - 1)*partition_size;
-
-        % Unfortunately the sample size may not parition perfectly. 
-        % While most rookies would use an ugly looking if statement
-        % it is preferable to add the final loop iteration (which would 
-        % contain the remainder) explicitly. 
-
-        % Set aside the data to exercise the generated tree
-        test_examples = examples(lower_bound + 1 : length(examples), :);
-
-        % Grab the data to generate the tree
-        training_examples = examples(1 : (n - 1)*partition_size, :);
-        
-        training_classifications = classifications(1:(n - 1) * ...
-            partition_size, :);
-        
-        emotion_decision_trees = ...
-            generate_all_trees(training_examples, ...
-                               training_classifications,...
-                               possible_outcomes);
-
-        %Concatinate the classifications
-        classifcation_results(((n - 1)*partition_size+1) : len,:) =... 
-            classify(emotion_decision_trees,  test_examples, strategy);
-
+    lower_bound = (n - 1)*partition_size;
+    
+    % Unfortunately the sample size may not parition perfectly.
+    % While most rookies would use an ugly looking if statement
+    % it is preferable to add the final loop iteration (which would
+    % contain the remainder) explicitly.
+    
+    % Set aside the data to exercise the generated tree
+    test_examples = examples(lower_bound + 1 : length(examples), :);
+    
+    test_classes = classifications(lower_bound + 1 : length(classifications), :);
+    
+    % Grab the data to generate the tree
+    training_examples = examples(1 : (n - 1)*partition_size, :);
+    
+    training_classifications = classifications(1:(n - 1) * ...
+        partition_size, :);
+    
+    emotion_decision_trees = ...
+        generate_all_trees(training_examples, ...
+        training_classifications,...
+        possible_outcomes);
+    
+    outcomes = classify(emotion_decision_trees,  test_examples, strategy);
+    
+    %Concatinate the classifications
+    classifcation_results(((n - 1)*partition_size+1) : len,:) =...
+        outcomes;
+    
+    confusionMatrix{n} = confusion_matrix(test_classes,outcomes, 6);
+    
     results = classifcation_results;
-
-    %Access an invidual tree by
-    %all_trees{1}(2)
 
 end
