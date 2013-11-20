259f6d0f50ff510e3f8585fee347c33aede1eed6
Marman1337 <marman1@gmail.com>
2013-11-20 14:24:44 +0000

    euclidean distance

--- 3_CBR/calculateDistance.m
+++ 3_CBR/calculateDistance.m
@@ -1,4 +1,16 @@
+% ------ MANHATTAN DISTANCE ------
+
 function distance = calculateDistance(a, b)
     assert(size(a, 2) == size(b,2));
     distance = sum(abs(a - b));
 end
+
+% ------ EUCLIDEAN DISTANCE ------
+
+%function distance = calculateDistance(a, b)
+%    assert(size(a, 2) == size(b,2));
+%    distance = sum(abs(a - b));
+%    distance = sqrt(distance);
+%end
+
+% ------ THIRD DISTANCE ------
