0edd45341cd7dbb2ce5675bafce3a330c3f18087
Yong Wen Chua <lawliet89@gmail.com>
2013-11-28 16:27:35 +0000

    Make CalculateDistance be selectable

--- 3_CBR/calculateDistance.m
+++ 3_CBR/calculateDistance.m
@@ -1,16 +1,28 @@
+function distance = calculateDistance(a, b, method)
+    if nargin < 3
+       method = 1;
+    end
+
+    if method == 1
+        distance = calculateDistanceManhattan(a, b);
+    else
+        distance = calculateDistanceEuclidean(a, b);
+    end
+end
+
 % ------ MANHATTAN DISTANCE ------
 
-function distance = calculateDistance(a, b)
+function distance = calculateDistanceManhattan(a, b)
     assert(size(a, 2) == size(b,2));
     distance = sum(abs(a - b));
 end
 
 % ------ EUCLIDEAN DISTANCE ------
 
-%function distance = calculateDistance(a, b)
-%    assert(size(a, 2) == size(b,2));
-%    distance = sum(abs(a - b));
-%    distance = sqrt(distance);
-%end
+function distance = calculateDistanceEuclidean(a, b)
+    assert(size(a, 2) == size(b,2));
+    distance = sum(abs(a - b));
+    distance = sqrt(distance);
+end
 
 % ------ THIRD DISTANCE ------
